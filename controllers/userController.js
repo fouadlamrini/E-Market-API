d6b895185e9144c844a44755a29e12fbc39720a9
fouad lmrini <fouad11.lamrini@gmail.com>
2025-10-09 10:16:40 +0100

    function deletUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,5 +41,21 @@ async getUserById(req, res) {
     }
 }
 
+async deleteUser(req, res) {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
 }
 module.exports = UserController;
